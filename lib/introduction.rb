61bdf0cc05bfd35b3f963b529ddc1790d983b442
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-06 08:58:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,12 +12,14 @@ end
 
 
 ## Define a Method That Takes in and Uses Two Arguments
-
 # Define a method, `#introduction_with_language` that takes in two arguments,
 # `name` and `language` and outputs the phrase: "Hi, my name is #{name} and I am
 # learning to program in #{language}."
 
 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
 
