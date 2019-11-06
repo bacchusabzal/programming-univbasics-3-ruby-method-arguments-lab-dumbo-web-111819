ea08d291c6c28d3da90d415de3f7b925ff51830a
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-06 09:16:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -22,7 +22,7 @@ def introduction_with_language_optional(name, language ="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional(Abzal)
+introduction_with_language_optional("Abzal")
 
 
 
